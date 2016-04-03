31ad3966271c3392a848eccff213462afcfde9fe
shaundashjian <shaundashjian@yahoo.com>
2016-04-02 22:43:55 -0600

    Objects and Arrays: 2nd and 3rd exercises solved

--- exercises/chapter_4.js
+++ exercises/chapter_4.js
@@ -23,3 +23,127 @@ function sum(array) {
 }
 
 // Reversing an array
+function reverseArray(array) {
+	var reverse = [];
+	for(var i = 0; i < array.length; i++)
+		reverse.unshift(array[i]);
+	return reverse;
+}
+
+function reverseArrayInPlace(array) {
+	for(var i = 0; i < array.length/2; i++) {
+		var temp = array[i];
+		array[i] = array[array.length-1-i];
+		array[array.length-1-i] = temp;
+	}
+	return array;
+}
+
+// A List
+var list = {
+	value: 1,
+	rest: {
+		value: 2,
+		rest: {
+			value: 3,
+			rest: null
+		}
+	}
+};
+
+function arrayToList(array) {
+	var list = {};
+	list = {
+		value: array[array.length-1],
+		rest: null
+	};
+	for (var i = array.length-2; i >=0; i--)
+		prepend(array[i], list);
+	return list;
+}
+
+function listToArray(list) {
+	var array = [];
+	do {
+		array.push(list.value);
+		list = list.rest;	
+	} while (list != null) ;
+	return array;
+}
+
+function prepend(element, list) {
+	list = {
+			value: element,
+			rest: list
+		}	
+	return list;
+}
+
+function nth(list, index) {
+	for (var i = 0; i < index; i++) {
+		list = list.rest;	
+		if (list == null)
+			break;
+	}
+	if (i == index) {
+		return list.value;
+	} else {
+		return undefined;
+	}
+}
+
+// Recursive version of nth
+function nth(list, index) {
+	if (index == 0 && list != null) {
+		return list.value;	
+	} else if (list == null){
+		return undefined;
+	} else {
+		return nth(list.rest, index-1);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
